71f66314885e9e39679291d798a1c8c31afa409b
Nandhakumar2550 <nm8573@srmist.edu.in>
2026-05-11 15:23:08 +0530

    [Nandhakumar]/UC3_Insert_values

---
+++ UC3_Insert_Values.sql
@@ -0,0 +1,9 @@
+use address_book_db;
+INSERT INTO AddressBook
+(first_name, last_name, address, city, state, zip, phone_number, email)
+VALUES
+('Nandha', 'Kumar', 'Anna Nagar', 'Chennai', 'Tamil Nadu', 600001, 9876543210, 'nandha@gmail.com'),
+
+('Arun', 'Raj', 'T Nagar', 'Chennai', 'Tamil Nadu', 600002, 9876543211, 'arun@gmail.com'),
+
+('Kavin', 'M', 'MG Road', 'Bangalore', 'Karnataka', 560001, 9876543212, 'kavin@gmail.com');
